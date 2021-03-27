d8737fab08a5afc7a4a2bc638c9c513e3e9880c9
anamos21 <43624214+anamos21@users.noreply.github.com>
2021-03-27 14:21:04 -0400

    TS02: routes and corresponding views added

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home():
+    return 
+
+def login():
+    return 
+
+def ipdash():
+    return
+
+def searchresults():
+    return 
+
+def itemdetails():
+    return 
+
+def addequipment():
+    return 
+
+def homeuseform():
+    return 
+
+def networkform():
+    return
+
+def searchqueryform():
+    return
