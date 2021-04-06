c78fd7727726ca616b823e5ea8f726207f165b13
despinoza17 <despi17@vt.edu>
2021-04-06 19:19:54 -0400

    Add search results page

--- inventory/static/js/searchEquipments.js
+++ inventory/static/js/searchEquipments.js
@@ -3,7 +3,11 @@ const searchField=document.querySelector("#searchField");
 searchField.addEventListener("keyup", (e)=>{
     const searchValue=e.target.value;
 
-    if (searchValue.length > 0) {
+    if (searchValue.trim().length > 0) {
         console.log('Search value: ', searchValue);
+
+        fetch("")
+
+
     }
 })
--- inventory/views.py
+++ inventory/views.py
@@ -2,22 +2,26 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import Permission, User
 from django.contrib.auth import authenticate, login, logout
 from .models import Equipment
-from django.http import JsonResponse
-import json
+from django.db.models import Q
 
 
 
 # Create your views here.
 def home_view(request):
-    if request.method == 'POST':
-        search_str = json.loads(request.body).get('searchText')
-        equipments = Equipment.filter(manufacturer_model__starts_with=search_str)
-        data = equipments.values()
-
-        return JsonResponse(list(data), safe=False)
-
-
-    return render(request, 'inventory/home.html')
+    # Handles search query
+    context = {}
+    context['options'] = {"VT Tag": "vttag", "CS Tag": "cstag", "Serial Number": "serial_number",
+                            "Manufacturer": "manufacturer_model", "Custodian": "custodian", "Hostname": "hostname",
+                            "Building": "building", "IP": "ip"}
+    query = ""
+    selected_option = ""
+    if request.method == "GET":
+        query = request.GET.get('search')
+        selected_option = request.GET.get('search_options')
+        context["query"] =  query
+        context["selected_option"] = selected_option
+    context['equipments'] = get_equipment_queryset(query, selected_option)
+    return render(request, 'inventory/home.html', context)
 
 def login_view(request):
     if request.method == 'POST':
@@ -27,7 +31,7 @@ def login_view(request):
         if user is not None:
             login(request, user)
             return redirect('/home')
-    return render(request, 'auth/login.html')
+    return render(request, 'inventory/login.html')
 
 def logout_view(request):
     logout(request)
@@ -36,9 +40,6 @@ def logout_view(request):
 
 def ipdash_view(request):
     return render(request, 'events/ip-dashboard.html')
-
-def searchresults_view(request):
-    return render(request, 'events/index.html')
 
 def itemdetails_view(request):
     return render(request, 'events/index.html')
@@ -54,3 +55,65 @@ def networkform_view(request):
 
 def searchqueryform_view(request):
     return render(request, 'events/index.html')
+
+def get_equipment_queryset(query=None, filter=None):
+    if query:
+        queryset = []
+        queries = query.split(" ")
+        if filter == 'vttag':
+            for q in queries:
+                equipments = Equipment.objects.filter(Q(vttag__icontains=q)).distinct()
+                for equipment in equipments: 
+                    queryset.append(equipment)
+        elif filter == 'cstag':
+            for q in queries:
+                equipments = Equipment.objects.filter(Q(cstag__icontains=q)).distinct()
+                for equipment in equipments: 
+                    queryset.append(equipment)
+        elif filter == 'serial_number':
+            for q in queries:
+                equipments = Equipment.objects.filter(Q(serial_number__icontains=q)).distinct()
+                for equipment in equipments: 
+                    queryset.append(equipment)
+        elif filter == 'manufacturer_model':
+            for q in queries:
+                equipments = Equipment.objects.filter(Q(manufacturer_model__icontains=q)).distinct()
+                for equipment in equipments: 
+                    queryset.append(equipment)
+        elif filter == 'custodian': 
+            for q in queries:
+                equipments = Equipment.objects.filter(Q(custodian__icontains=q)).distinct()
+                for equipment in equipments: 
+                    queryset.append(equipment)
+        elif filter == 'hostname':
+            for q in queries:
+                equipments = Equipment.objects.filter(Q(hostname__icontains=q)).distinct()
+                for equipment in equipments: 
+                    queryset.append(equipment)
+        elif filter == 'building':
+            for q in queries:
+                equipments = Equipment.objects.filter(Q(building__icontains=q)).distinct()
+                for equipment in equipments: 
+                    queryset.append(equipment)
+        elif filter == 'ip':
+            for q in queries:
+                equipments = Equipment.objects.filter(Q(ip__icontains=q)).distinct()
+                for equipment in equipments: 
+                    queryset.append(equipment)
+        else:
+            for q in queries:
+                equipments = Equipment.objects.filter(
+                    Q(vttag__icontains=q) |
+                    Q(cstag__icontains=q) |
+                    Q(serial_number__icontains=q) |
+                    Q(manufacturer_model__icontains=q) |
+                    Q(custodian__icontains=q) |
+                    Q(hostname__icontains=q) |
+                    Q(building__icontains=q) |
+                    Q(ip__icontains=q)
+                ).distinct()
+                for equipment in equipments: 
+                    queryset.append(equipment)
+        return list(set(queryset))
+    else:
+        return Equipment.objects.all()
