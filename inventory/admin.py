a2e494a7294c2f61011d8c0ba39a1bcfaf778228
despinoza17 <despi17@vt.edu>
2021-04-08 21:33:44 -0400

    Added tables to django admin

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from .models import Equipment
+from .models import *
 
 # Register your models here.
 admin.site.register(Equipment)
+admin.site.register(IP)
+admin.site.register(History)
+admin.site.register(Checkout)
+admin.site.register(Building)
