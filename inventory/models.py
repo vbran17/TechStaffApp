9855f21d1ac05b1b5c31c41979dfc7931ae064ed
despinoza17 <despi17@vt.edu>
2021-04-18 22:00:17 -0400

    Added Inventory User Model

--- inventory/models.py
+++ inventory/models.py
@@ -1,16 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+'''
 class PersonalID(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     office = models.CharField(max_length=255)
     email = models.EmailField(max_length=255)
     phone = models.CharField(max_length=255)
+'''
+class InventoryUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    pid = models.CharField(max_length=255, blank=True, null=True)
 
 class Building(models.Model):
     name = models.CharField(max_length=255, blank=True, null=True)
     ipv6_prefix = models.CharField(max_length=255, blank=True, null=True)
+    def __str__(self):
+        return self.name
 
 class IP(models.Model):
     IPv4='I4'
@@ -35,7 +44,7 @@ class Hostname(models.Model):
 
 class Equipment(models.Model):
     vt_tag = models.CharField(max_length=255, blank=True)
-    building = models.ForeignKey(Building, blank=True, null=True, on_delete=models.CASCADE)
+    building = models.ForeignKey(Building, blank=True, null=True, on_delete=models.RESTRICT)
     room = models.CharField(max_length=15, blank=True)
     manufacturer_model = models.CharField(max_length=255, blank=True)
     serial_number = models.CharField(max_length=255, blank=True)
@@ -46,16 +55,16 @@ class Equipment(models.Model):
     purchase_date = models.DateField(blank=True)
     purchase_value = models.DecimalField(max_digits=6, decimal_places=2)
     acquisition_date = models.DateField(blank=True)
-    hostname = models.ForeignKey(Hostname, on_delete=models.RESTRICT, blank=True)
+    hostname = models.ForeignKey(Hostname, on_delete=models.RESTRICT, blank=True, related_name="Hostname")
     dept = models.CharField(max_length=10, blank=True)
     status = models.CharField(max_length=128, blank=True)
-    mail_exchange = models.SmallIntegerField(blank=True)
+    mail_exchange = models.ForeignKey(Hostname, on_delete=models.RESTRICT, blank=True, related_name="MailExchangeHostname")
     #have to change mail exchange to hostname 
     cs_tag = models.CharField(max_length=255, blank=True)
     notes = models.CharField(max_length=10000, blank=True)
 
 class Checkout(models.Model):
-    contact = models.ForeignKey(PersonalID, on_delete=models.RESTRICT, blank=True)
+    contact = models.ForeignKey(InventoryUser, on_delete=models.RESTRICT, blank=True)
     equipment = models.ForeignKey(Equipment, on_delete=models.RESTRICT, blank=True)
     checkoutdate = models.DateField(blank=True, auto_now_add=True)
 
