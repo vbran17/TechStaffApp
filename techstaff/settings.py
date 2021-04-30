6706a351848be6510729d7f093fe75d85366740e
despinoza17 <despi17@vt.edu>
2021-04-30 15:04:03 -0400

    Fixed password validation

--- techstaff/settings.py
+++ techstaff/settings.py
@@ -97,16 +97,6 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-    
 ]
 
 
